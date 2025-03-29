6e69b28419baf98affe2449b966a088985674a53
WandesonAndrade <wandesonandrade33@gmail.com>
2025-03-29 10:43:48 -0300

    atualização do arquivo de inicialização do venom2

--- src/config/venom.js
+++ src/config/venom.js
@@ -16,8 +16,7 @@ const getStatus = () => currentStatus;
 async function initializeVenom() {
   const browser = await puppeteer.launch({
     headless: "new",
-    executablePath:
-      "/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.126/chrome-linux64/chrome", // Usa o Chrome já instalado no Render
+
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
